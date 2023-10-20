dd6d92e02f9611423d0ba04fc13fe70753ef9060
UltiRed8 <84141770+UltiRed8@users.noreply.github.com>
2023-10-20 16:16:47 +0200

    Merged with main

--- Project_RPG_TourParTour/Project_RPG_TourParTour/Main.cpp
+++ Project_RPG_TourParTour/Project_RPG_TourParTour/Main.cpp
@@ -6,16 +6,18 @@ using namespace std;
 void DisplayList(string _text, const string _list[], int _sizeOfList, bool _printIndexes);
 void Display(string _text);
 void DisplayValue(string _text, int _value);
-bool CombatLoop(int _currentHealth, int _maxHealth, int _weaponId, int _enemyId, string _enemies[], int _enemiesStats[][3], string _weapons[], int _weaponsStats[][3], int _healItemAmount, bool _playerInventory[], int _weaponsAmount);
+bool CombatLoop(int& _currentHealth, int _maxHealth, int _weaponId, int _enemyId, string _enemies[], int _enemiesStats[][3], string _weapons[], int _weaponsStats[][3], int& _healItemAmount, bool _playerInventory[], int _weaponsAmount);
 int IntInput(string _question, int _min, int _max);
 void DisplayWeapons(string _text, const string _weapons[], int _weaponsAmount, bool _printIndexes, bool _showInventoryItems, const bool _hasItem[]);
 void GameLoop();
-void Shop(int& _playerMoney, string* _weapons, int _weaponsAmount, bool _weaponsInInventory[], int _weaponsStats[][3]);
+void Shop(int& _playerMoney, string* _weapons, int _weaponsAmount, bool _weaponsInInventory[], int _weaponsStats[][3], int& _healItemAmount);
 int Inventory(bool _playerInventory[], int _weaponsAmount, string _weapons[]);
 
 int main()
 {
+	Display("\x1B[93m========================");
 	Display("BIENVENUE DANS L'ARENE !");
+	Display("========================");
 	GameLoop();
 	return 0;
 }
@@ -25,76 +27,82 @@ void GameLoop()
 {
 	const int _weaponsAmount = 2, _enemiesAmount = 1;
 	int _playerHealth = 10, _playerMaxHealth = 10;
-	int _playerMoney = 1000;
+	int _playerMoney = 0;
 	int _playerWeaponId = 0;
 	int _healItemAmount = 0;
 	bool _weaponsInInventory[_weaponsAmount]{ true };
 
 	string _weapons[_weaponsAmount], _enemies[_enemiesAmount];
-	int _weaponsStats[_weaponsAmount][3]; // 0=> Dégats  1=> Precision  2=> Prix shop
-	int _enemiesStats[_enemiesAmount][3]; // 0=> Vie  1=> Dégats  2=>Pièces
+	int _weaponsStats[_weaponsAmount][3]; // 0=> Degats  1=> Precision  2=> Prix shop
+	int _enemiesStats[_enemiesAmount][3]; // 0=> Vie  1=> Degats  2=>Pièces
 
-	_weapons[0] = "Arme 1"; _weaponsStats[0][0] = 5; _weaponsStats[0][1] = 75; _weaponsStats[0][2] = 0; // Arme 1 5dégats 75%précision 0prix
-	_weapons[1] = "Arme 2"; _weaponsStats[1][0] = 10; _weaponsStats[1][1] = 50; _weaponsStats[1][2] = 10; // Arme 2 10dégats 50%précision 10prix
+	_weapons[0] = "Arme 1"; _weaponsStats[0][0] = 5; _weaponsStats[0][1] = 75; _weaponsStats[0][2] = 0; // Arme 1 5degats 75%precision 0prix
+	_weapons[1] = "Arme 2"; _weaponsStats[1][0] = 10; _weaponsStats[1][1] = 50; _weaponsStats[1][2] = 10; // Arme 2 10degats 50%precision 10prix
 
-	_enemies[0] = "Enemi 1"; _enemiesStats[0][0] = 6; _enemiesStats[0][1] = 3; _enemiesStats[0][2] = 8; // Enemi 1 6pv 3dégats 8pièces
+	_enemies[0] = "Enemi 1"; _enemiesStats[0][0] = 6; _enemiesStats[0][1] = 3; _enemiesStats[0][2] = 8; // Enemi 1 6pv 3degats 8pièces
 
 	string _reponse;
 	bool _returnMenu = true;
 	do
 	{
-		Display("Ton argent : "); cout << _playerMoney << endl;
-		Display("Rentre dans l'arene -> combat \n");
-		Display("Ton Inventaire -> inventaire \n");
-		Display("Veux tu aller au shop ? -> shop \n");
+		Display("\x1B[90m========MENU PRINCIPALE========\x1B[37m");
+		DisplayValue(">>> Votre argent : ", _playerMoney);
+		DisplayValue(">>> Votre vie : ", _playerHealth);
+		Display("\x1B[90m========\x1B[37m");
+		Display("- Combat => Faire un combat");
+		Display("- Inventaire => Changer d'arme");
+		Display("- Shop => Ouvrir le shop\x1B[96m");
 		cin >> _reponse;
-		if (_reponse == "combat")
+		if (_reponse == "Combat")
 		{
 			system("cls");
 			//choix du combat
-			DisplayList("Liste d'ennemie : ", _enemies, 1, true);
-			int _enemyId = IntInput("Rentrer ID de l'ennemi à affronter", 0, 1);
+			
+			Display("\x1B[90m========LISTE ENEMIS========\x1B[37m");
+			DisplayList("", _enemies, 1, true);
+			Display("\x1B[90m========\x1B[37m");
+			int _enemyId = IntInput("\x1B[37mRentrer ID de l'ennemi a affronter : \x1B[96m", 0, 1);
 
 			if (CombatLoop(_playerHealth, _playerMaxHealth, _playerWeaponId, _enemyId, _enemies, _enemiesStats, _weapons, _weaponsStats, _healItemAmount, _weaponsInInventory, _weaponsAmount)) // si il gagne recompense
 			{
 				_playerMoney += _enemiesStats[_enemyId][2];
 			}
 		}
-		else if (_reponse == "inventaire")
+		else if (_reponse == "Inventaire")
 		{
-			//OpenInventaire(); //focntion reutilisable meme en combat
-
+			_playerWeaponId = Inventory(_weaponsInInventory, _weaponsAmount, _weapons);
 		}
-		else if (_reponse == "shop")
+		else if (_reponse == "Shop")
 		{
 
-			Shop(_playerMoney, _weapons, _weaponsAmount, _weaponsInInventory, _weaponsStats);
+			Shop(_playerMoney, _weapons, _weaponsAmount, _weaponsInInventory, _weaponsStats, _healItemAmount);
 		}
 		else
 		{
-			cout << "Choix invalide" << endl;
+			Display("\x1B[91mChoix invalide");
 		}
 	} while (_returnMenu);
 }
 
-bool CombatLoop(int _currentHealth, int _maxHealth, int _weaponId, int _enemyId, string _enemies[], int _enemiesStats[][3], string _weapons[], int _weaponsStats[][3], int _healItemAmount, bool _playerInventory[], int _weaponsAmount) {
+bool CombatLoop(int& _currentHealth, int _maxHealth, int _weaponId, int _enemyId, string _enemies[], int _enemiesStats[][3], string _weapons[], int _weaponsStats[][3], int& _healItemAmount, bool _playerInventory[], int _weaponsAmount) {
 	bool _shouldLoop = true, _actionLoop = true;
 	bool _won = false;
-	Display("Début du combat contre" + _enemies[_enemyId] + " !");
+	Display("\x1B[91mDebut du combat contre \x1B[31m" + _enemies[_enemyId] + "\x1B[91m !\x1B[37m");
 	int _enemyLife = _enemiesStats[_enemyId][0];
 	int _enemyDamages = _enemiesStats[_enemyId][1];
 	string _actions[] = { "Attaquer", "Changer d'arme", "Utiliser soin" };
 	do {
-		DisplayValue("Vie de l'enemi: ", _enemyLife);
-		DisplayValue("Votre vie: ", _currentHealth);
-		DisplayList("Séléctionnez l'action a effectuer :", _actions, 3, true);
+		DisplayValue(">>>Vie de l'enemi: ", _enemyLife);
+		DisplayValue(">>>Votre vie: ", _currentHealth);
+		Display("\x1B[90m========\x1B[37m");
+		DisplayList("\x1B[37mSelectionnez l'action a effectuer :", _actions, 3, true);
 		_actionLoop = true;
 		do {
-			switch (IntInput("Entrez le numéro de l'action : ", 0, 3)) {
+			switch (IntInput("\x1B[37mEntrez le numero de l'action : \x1B[96m", 0, 3)) {
 			case 0:
 				_actionLoop = false;
 				_enemyLife -= _weaponsStats[_weaponId][0];
-				DisplayValue("Vous avez attaqué l'enemi : ", _weaponsStats[_weaponId][0]);
+				DisplayValue("\x1B[92mVous avez attaque l'enemi : \x1B[91m", _weaponsStats[_weaponId][0]);
 				break;
 			case 1:
 				_weaponId = Inventory(_playerInventory, _weaponsAmount, _weapons);
@@ -104,8 +112,8 @@ bool CombatLoop(int _currentHealth, int _maxHealth, int _weaponId, int _enemyId,
 				if (_healItemAmount - 1 >= 0) {
 					_healItemAmount--;
 					_currentHealth = _maxHealth;
-				} else Display("Vous n'avez pas d'item de soin!");
-				_actionLoop = false;
+					_actionLoop = false;
+				} else Display("\x1B[91mVous n'avez pas d'item de soin!");
 				break;
 			default:
 				break;
@@ -114,21 +122,23 @@ bool CombatLoop(int _currentHealth, int _maxHealth, int _weaponId, int _enemyId,
 		if (_enemyLife <= 0) _won = true;
 		if (!_won) {
 			_currentHealth -= _enemyDamages;
-			Display("L'enemi vous as attaqué, vous perdez " + to_string(_enemyDamages) + " points de vie!");
+			Display("\x1B[33mL'enemi vous as attaque, vous perdez \x1B[31m" + to_string(_enemyDamages) + "\x1B[33m points de vie!");
 		}
 		if (_won || _currentHealth == 0) _shouldLoop = false;
 		system("PAUSE");
 		system("cls");
 	} while (_shouldLoop);
-	if (_won) Display("Tu as gagné le combat!");
-	else Display("Tu as perdu le combat!");
+	if (_won) Display("\x1B[92mTu as gagne le combat!");
+	else Display("\x1B[31mTu as perdu le combat!");
 	return _won;
 }
 
 int Inventory(bool _playerInventory[], int _weaponsAmount, string _weapons[]) {
 	int _wantedWeapon = 0;
+	Display("\x1B[90m========INVENTAIRE========\x1B[37m");
 	DisplayWeapons("Vos armes :", _weapons, _weaponsAmount, true, true, _playerInventory);
-	_wantedWeapon = IntInput("Entrez l'id de l'arme que vous souhaitez utiliser : ", 0, _weaponsAmount);
+	Display("\x1B[90m========\x1B[37m");
+	_wantedWeapon = IntInput("\x1B[37mEntrez l'id de l'arme que vous souhaitez utiliser :\x1B[96m", 0, _weaponsAmount);
 	return _wantedWeapon;
 }
 
@@ -160,25 +170,41 @@ void DisplayList(string _text, const string _list[], int _sizeOfList, bool _prin
 	if (_text != "") cout << _text << endl;
 	for (int _i = 0; _i < _sizeOfList; _i++) {
 		if (_printIndexes) cout << _i;
-		cout << "- " << _list[_i] << endl;
+		cout << " => " << _list[_i] << endl;
 	}
 }
 
-void Shop(int& _playerMoney, string* _weapons, int _weaponsAmount, bool _weaponsInInventory[], int _weaponsStats[][3])
+void Shop(int& _playerMoney, string* _weapons, int _weaponsAmount, bool _weaponsInInventory[], int _weaponsStats[][3], int& _healItemAmount)
 {
 	bool _wantContinue = true;
 	int _idChoose;
 	do
 	{
 		system("cls");
+		Display("\x1B[90m========SHOP========\x1B[37m");
 		DisplayValue("Ton argent est : ", _playerMoney);
-		DisplayWeapons("Voici le stock : ", _weapons, _weaponsAmount, true, false, _weaponsInInventory);
-		Display("-1 pour quitter le shop");
+		Display("\x1B[90m========\x1B[37m");
+		DisplayWeapons("Disponible : ", _weapons, _weaponsAmount, true, false, _weaponsInInventory);
+		Display("-1 => Pour acheter potion de soin");
+		Display("-2 => Pour quitter le shop");
+		Display("\x1B[90m========\x1B[96m");
 
 		cin >> _idChoose;
-		if (_idChoose == -1)
+		if (_idChoose == -2)
 		{
 			_wantContinue = false;
+			system("cls");
+		}
+		else if (_idChoose == -1)
+		{
+			if (_playerMoney < 10)
+			{
+				Display("\x1B[91mT'es trop pauvre!");
+				system("PAUSE");
+				continue;
+			}
+			_healItemAmount++;
+			_playerMoney -= 10;
 		}
 		else
 		{
@@ -188,9 +214,14 @@ void Shop(int& _playerMoney, string* _weapons, int _weaponsAmount, bool _weapons
 			}
 			else
 			{
+				if (_playerMoney < _weaponsStats[_idChoose][2])
+				{
+					Display("\x1B[91mT'es trop pauvre!");
+					system("PAUSE");
+					continue;
+				}
 				_weaponsInInventory[_idChoose] = true;
 				_playerMoney -= _weaponsStats[_idChoose][2];
-				cout << _weaponsStats[_idChoose][2];
 				system("PAUSE");
 			}
 		}
@@ -204,7 +235,7 @@ void DisplayWeapons(string _text, const string _weapons[], int _weaponsAmount, b
 	for (int _i = 0; _i < _weaponsAmount; _i++) {
 		if (_hasItem[_i] == _showInventoryItems) {
 			if (_printIndexes) cout << _i;
-			cout << "- " << _weapons[_i] << endl;
+			cout << " => " << _weapons[_i] << endl;
 		}
 	}
 }
